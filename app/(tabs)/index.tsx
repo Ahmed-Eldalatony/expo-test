e0c038c523b6670bb82a1df9f587d615e3deaf72
Ahmed-Eldalatony <a01141399246@gmail.com>
2025-03-20 07:57:38 +0200

    test simple local notification approach

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 // app/_layout.tsx or app/entry.tsx
-import React, { useEffect, useState } from 'react';
-import {Text, View, ActivityIndicator, Button, TouchableOpacity } from 'react-native';
-import { storage } from '../storage';
+import React, { useEffect, useState, useRef } from 'react';
+import { Text, View, ActivityIndicator, Button, TouchableOpacity, Alert } from 'react-native';
 import { useRouter } from 'expo-router';
 import { colorScheme, useColorScheme } from "nativewind";
-// import { ThemedText } from '@/components/ThemedText';
-// import Text from 'react-native';
+import { initializeNotifications } from '@/lib';
+import { storage } from '../storage';
+import * as Notifications from 'expo-notifications';
+
+initializeNotifications();;
 
 export default function Entry() {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   const { setColorScheme } = useColorScheme();
-
+  const notificationIdentifierRef = useRef<string | null>(null);
   useEffect(() => {
     // MMKV is synchronous so we can check immediately
     const seen = storage.getString('hasSeenOnboarding');
@@ -22,6 +24,29 @@ export default function Entry() {
     setLoading(false);
   }, []);
 
+
+  const scheduleNotification = async (seconds = 1) => {
+    const identifier = await Notifications.scheduleNotificationAsync({
+      content: { title: "Test", body: "Test body" },
+      trigger: { seconds: 2 },
+    });
+    return identifier;
+  };
+  const handleShowNotification = () => {
+    scheduleNotification();
+  };
+
+  const handleScheduleNotification = async () => {
+    const id = await scheduleNotification(15);
+    notificationIdentifierRef.current = id;
+  };
+
+  const handleCancelScheduledNotification = () => {
+    const identifier = notificationIdentifierRef.current;
+    if (!identifier) return;
+    Notifications.cancelScheduledNotificationAsync(identifier);
+  };
+
   if (loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
@@ -30,15 +55,23 @@ export default function Entry() {
     );
   }
 
- // Render your main app layout (e.g., Tabs) once loading is complete
+  // Render your main app layout (e.g., Tabs) once loading is complete
   return (
     <View className="flex-1 justify-center items-center bg-white dark:bg-primary-900">
       {/* <ThemedText>Test Text</ThemedText> */}
-      <TouchableOpacity className="dark:bg-primary-800 "  onPress={(prev) => colorScheme.toggle()} >
+      <TouchableOpacity className="dark:bg-primary-800 " onPress={(prev) => colorScheme.toggle()} >
         <Text className='dark:text-white'>
           Toggle Theme
         </Text>
-      </TouchableOpacity>  
+        <Text onPress={() => router.push("/slide7")} className='dark:text-white'>
+          go 7
+        </Text>
+        <Button title="Show notification" onPress={handleShowNotification} />
+        <Button title="handle schedule" onPress={handleScheduleNotification} />
+        <Button title="handle schedule" onPress={handleCancelScheduledNotification} />
+      </TouchableOpacity>
     </View>
   );
 }
+
+
